cf1fa5e3540e8a4c9341529c59418f3bdf828075
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-03-16 14:40:26 +0530

    form

--- restaurant-app/src/components/Meals/Items.js
+++ restaurant-app/src/components/Meals/Items.js
@@ -3,12 +3,25 @@ import "./Items.css"
 
 const Items = (props) => {
    return(
+    <>
+    <div className="meal">
     <div>
     <h2>{props.items.dish}</h2>
     <p>{props.items.description}</p>
     <h3>$ {props.items.price}</h3>
-    <hr />
     </div>
+    <div className="amount">
+        <form>
+            <label className="head">Amount </label>
+            <input type="text" value="1" size="3"/>
+            <br/>
+            <br/>
+            <button>+ Add</button>
+        </form>
+    </div>
+    </div>
+    <hr />
+    </>
    )
 }
 
